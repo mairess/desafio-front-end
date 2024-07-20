cad2f4ca29f17785d94a122815d6ec99da710007
mairess <trabalho.maires@gmail.com>
2024-07-20 17:40:04 -0300

    feat: add search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <Header /> } />
-      <Route path="/search" element={ <SearchBar /> } />
-    </Routes>
+    <>
+      <Header />
+      <div
+        className="flex flex-col items-center px-spacing-regular-20"
+      >
+        <SearchBar />
+      </div>
+    </>
   );
 }
 
